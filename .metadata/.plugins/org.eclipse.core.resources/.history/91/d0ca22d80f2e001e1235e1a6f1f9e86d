6ed620f9d411fa801137fedb4c8aa9f8e800a675
Luiz domingo da silva silva <aulasead2019@gmail.com>
2023-07-29 10:05:26 -0300

    Criado o construtor da class Pessoa

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/91/d0ca22d80f2e001e1235e1a6f1f9e86d
@@ -0,0 +1,11 @@
+package com.Luiz.domingo;
+
+public abstract class Pessoa {
+	
+	private String name;
+	private String telefone;
+	private String endereco;
+	
+	public abstract double calcularImposto();
+	
+}
--- Livro_Deitel/src/com/Luiz/domingo/Pessoa.java
+++ Livro_Deitel/src/com/Luiz/domingo/Pessoa.java
@@ -6,6 +6,17 @@ public abstract class Pessoa {
 	private String telefone;
 	private String endereco;
 	
+	/* Criando o metodo contrutor da class Pessoa */
+	public Pessoa(String name, String telefone, String endereco)
+	{
+		this.name = name;
+		this.telefone = telefone;
+		this.endereco = endereco;
+	}
+	
+	
+	
+	//Metodo que ira calcular os imposto
 	public abstract double calcularImposto();
 	
 }
