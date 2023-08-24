2a9c9e7a3f976b1e770acd133357883eba664078
Luiz domingo da silva silva <aulasead2019@gmail.com>
2023-08-24 20:18:22 -0300

    Realizado o Exercicio Aprovados e DadosPessoais

--- Livro_Deitel/src/com/exercicios/Curso/Java/Udemy/DadosPessoais.java
+++ Livro_Deitel/src/com/exercicios/Curso/Java/Udemy/DadosPessoais.java
@@ -1,4 +1,8 @@
 package com.exercicios.Curso.Java.Udemy;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  * @author luiz-
  * Tem-se um conjunto de dados contendo a altura e o gênero (M, F) de N pessoas.
@@ -9,11 +13,79 @@ public class DadosPessoais
 {
     public static void main(String[] args)
     {
-        DadosPersonas();
+        DadosPerson();
     }
     
-    static void DadosPersonas()
+    static void DadosPerson()
     {
+    	Locale.setDefault(Locale.US);
+    	Scanner sc = new Scanner(System.in);
+    	
+    	System.out.print("Quantas Pessoas Irá Cadastrar? ");
+    	int quantidade = sc.nextInt();
+        System.out.println("-------------------------------");
+        
+        double maiorAltura = 0, mediaAlturaMulheres = 0, menorAltura = 0;
+        int contador = 0, quantidadeHomens = 0, quantidadeMulheres = 0;
+        
+    	double [] altura = new double[quantidade];
+        char [] genero = new char[quantidade];
+        
+        while(contador < quantidade)
+        {
+        	System.out.print("Digite a Altura da " + (contador + 1) + "° Pessoa: ");
+        	altura[contador] = sc.nextDouble();
+        	
+        	System.out.print("Digite o gênero da "  + (contador + 1) + "° Pessoa: ");
+        	genero[contador] = sc.next().charAt(0);
+        	System.out.println("");
+        	contador++;
+        }
+        
+        sc.close();
+        
+        System.out.println("-------------------------------");
+        System.out.println("");
+        
+        for(contador = 0; contador < quantidade; contador++)
+        {	
+        	if(altura[contador] > maiorAltura)
+        	{
+        		maiorAltura = altura[contador];
+        	}
+        	
+        	if(genero[contador] == 'M')
+        	{
+        		quantidadeHomens ++;
+        	}
+        	else
+        	{
+        		mediaAlturaMulheres += altura[contador];
+        		quantidadeMulheres ++;
+        	}
+        }
+        
+        contador = 0;
+        
+        while(contador < quantidade)
+        {
+        	menorAltura = altura[contador];
+        	for(int cont = 0; cont < quantidade; cont++)
+        	{
+        		if(altura[cont] < menorAltura)
+        		{
+        			menorAltura = altura[cont];
+        		}
+        	}
+        	contador ++;
+        }
+        
+        System.out.println("Exibindo os Dados....");
+        System.out.printf("A Maior Altura Do Grupo é: %.2f%n", maiorAltura);
+        System.out.printf("A Menor Altura Do Grupo é: %.2f%n", menorAltura);
+        System.out.println("A Quantidade de Homes no Grupo é De: "  + quantidadeHomens);
+        System.out.printf("A Média Das Altura das Mulheres é: %.2f%n", mediaAlturaMulheres / quantidadeMulheres);
+        System.out.println("Fim....");
         
     }
 }
--- Livro_Deitel/src/com/exercicios/Curso/Java/Udemy/ProblemaAprovados.java
+++ Livro_Deitel/src/com/exercicios/Curso/Java/Udemy/ProblemaAprovados.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  * Fazer um programa para ler um conjunto de N nomes de alunos,
  * bem como as notas que eles tiraram no 1º e 2º semestres. 
  * Cada uma dessas informações deve ser armazenada em um vetor.
- * Depois, imprimir os nomes dos alunos aprovados, 
+ * Depois, imprimir os nomes dos alunos aprovados. 
  * considerando aprovados aqueles cuja média das notas seja maior ou igual a 6.0 (seis).
  */
 public class ProblemaAprovados 
@@ -23,7 +23,8 @@ public class ProblemaAprovados
         Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
         
-        System.out.print("Quantos Alunos Irá Cadastrar ? ");
+        System.out.println("Digite Nome, Primeira e Segunda Nota Do Aluno. ");
+        System.out.print("Quantos Alunos Serão Cadastrados ? ");
         int quantidadeAlunos = sc.nextInt();
         
         double [] notasPrimeiroSemestre = new double[quantidadeAlunos];
@@ -31,27 +32,47 @@ public class ProblemaAprovados
         String nomes [] = new String [quantidadeAlunos];
         
         int contador = 0;
-        double media = 0;
+        double media = 0, soma = 0;
         
         System.out.println("--------------------------------");
         
         while( contador < quantidadeAlunos)
         {
             System.out.print("Digite o Nome Do " + (contador + 1) + "° Aluno: ");
-            String nome = sc.nextLine();
             sc.nextLine();
+            String nome = sc.nextLine();
             
             System.out.print("Digite a Nota Do 1° Semestre: ");
             double primeiroSemestre = sc.nextDouble();
             
             System.out.print("Digite a Nota Do 2° Semestre: ");
             double segundoSemestre = sc.nextDouble();
+            System.out.println("");
             
             nomes[contador] = nome;
             notasPrimeiroSemestre[contador] = primeiroSemestre;
             notasSegundoSemestre[contador] = segundoSemestre;
             contador++;
+            
             System.out.println("--------------------------------");
+            System.out.println("");
         }
+        sc.close();
+        
+        System.out.println("Alunos Aprovados....");
+        System.out.println("");
+        
+        for(contador = 0; contador < quantidadeAlunos; contador++) 
+        {
+        	soma =+ (notasPrimeiroSemestre[contador] + notasSegundoSemestre[contador]);
+        	media = soma / 2;
+        	
+        	if(media >= 6)
+        	{
+        		System.out.println(nomes[contador]);
+        	}
+        }
+        
+        System.out.println("Fim");  
     }
 }
